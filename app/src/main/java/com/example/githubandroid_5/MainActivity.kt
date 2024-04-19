b9c2fcf28f0d5a3591be1d2b374494a913020a74
Zununova <zununazk@gmail.com>
2024-04-19 15:43:49 +0300

    add something in activity

--- app/src/main/java/com/example/githubandroid_5/MainActivity.kt
+++ app/src/main/java/com/example/githubandroid_5/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.githubandroid_5
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        initialize()
+    }
+
+    private fun initialize() {
+        Log.e("main", "initialize: ", )
     }
 }
